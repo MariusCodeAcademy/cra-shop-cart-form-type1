7f7f06b6823019c64a958c3ecdbcc7dfa974e884
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-23 10:53:08 +0200

    items

--- src/components/Shop/Item.js
+++ src/components/Shop/Item.js
@@ -1,9 +1,16 @@
 import styled from 'styled-components';
 const ItemCard = styled.div`
+  display: flex;
+  /* flex-direction: column; */
+
+  flex-wrap: wrap;
   padding: 1rem;
   width: 28%;
   border: 1px solid #000;
   box-shadow: 3px 3px 7px lightgray;
+  & > * {
+    width: 100%;
+  }
   & h2 {
     font-size: 1rem;
   }
@@ -13,15 +20,20 @@ const ItemCard = styled.div`
     display: block;
     object-fit: contain;
   }
+  & button {
+    align-self: flex-end;
+  }
 `;
 
 function Item(props) {
-  const { title, price, image } = props.item;
+  const { title, price, image, category } = props.item;
   return (
     <ItemCard>
       <img src={image} alt={title} />
       <h2>{title}</h2>
       <h3>Price {price} eur</h3>
+      <p>Category: {category}</p>
+      <button>Add To cart</button>
     </ItemCard>
   );
 }
