d1a14bb985be0b1e50867da81288055423b0140d
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-23 09:57:28 +0200

    shop b4 axios

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -1,7 +1,20 @@
+import { useState, useEffect } from 'react';
+import List from '../components/Shop/List';
+
 function ShopPage() {
+  const [shopItems, setShopItems] = useState([]);
+
+  // ComponentDidMount
+  useEffect(() => {
+    getShopItems();
+  }, []);
+
+  const getShopItems = () => {};
+
   return (
     <main>
       <h1>Shop page</h1>
+      <List items={shopItems} />
     </main>
   );
 }
